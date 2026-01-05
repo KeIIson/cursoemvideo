6cce7f6f84b881d69fb79a060722fd9ec9f62d2d
Kellson <kelsonnb@gmail.com>
2026-01-05 11:28:17 -0300

    commit refinando desafio020

--- desafio020.py
+++ desafio020.py
@@ -1,1 +1,12 @@
-import math
+import random
+
+n1=str(input('Primeiro aluno: '))
+n2=str(input('Segundo nome: '))
+n3=str(input('Terceiro nome: '))
+n4=str(input('Quarto nome: '))
+
+lista=[n1, n2, n3, n4]
+random.shuffle(lista)
+
+print('A ordem de apresentação será')
+print(lista)
