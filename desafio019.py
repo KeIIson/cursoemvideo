4b37e96021fe426dfab450baa37136ef90584286
Kellson <kelsonnb@gmail.com>
2026-01-05 11:14:42 -0300

    commit refinando desafio019

--- desafio019.py
+++ desafio019.py
@@ -1,9 +1,11 @@
-import selectors
+from random import choice
 
-nome1=input('Digite o nome do primeiro aluno:')
-nome2=input('Digite o nome do segundo aluno:')
-nome3=input('Digite o nome do terceiro aluno:')
-nome4=input('Digite o nome do quarto aluno:')
+n1=str(input('Primeiro aluno:'))
+n2=str(input('Segundo aluno:'))
+n3=str(input('Terceiro aluno:'))
+n4=str(input('Quarto aluno:'))
 
-print(selectors, nome1,nome2,nome3,nome4))
-print(nome1)
+lista= [n1,n2,n3,n4]
+escolhido = choice(lista)
+
+print('O aluno escolhido foi {}!'.format(escolhido))
