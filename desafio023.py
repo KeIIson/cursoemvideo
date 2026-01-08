f2095f738d4e932149274220e10adf9a8f8a33a1
Kellson <kelsonnb@gmail.com>
2026-01-08 13:55:52 -0300

    commit refinando desafio023

--- desafio023.py
+++ desafio023.py
@@ -1,6 +1,14 @@
-num=str(input('Digite um valor entre 0 e 9999:'))
+"""num=str(input('Digite um valor entre 0 e 9999:'))
 #usar []  permite fatiar os números em uma string
 print('Milhar:',num[0])
 print('Centena:',num[1])
 print('Dezena:',num[2])
-print('Unidade:',num[3])
+print('Unidade:',num[3])"""
+
+#Convertendo INT para STRING antes do resultado
+num=int(input('Digite um número entre 0 e 9999:'))
+digitado=[int(n)for n in str(num)]
+print('Milhar:',digitado[0])
+print('Centena:',digitado[1])
+print('Dezena:',digitado[2])
+print('Unidade:',digitado[3])
