8ecd323aebf3fa0c2191f126e2c6d359f26cfee5
Kellson <kelsonnb@gmail.com>
2026-01-08 20:24:24 -0300

    commit desafio026 edt1.1

--- desafio026.py
+++ desafio026.py
@@ -2,8 +2,8 @@ print('Verificando quantidade de letras A:')
 a=str(input('Digite uma informação:'))
 
 
-print('A letra "A" aparece {} vezes'.format(a.count('a')))
-print('A posição do primeiro A:', a.find('A'))
+print('A letra "A" aparece {} vezes'.format(a.count('A')))
+print('A posição do primeiro "A":', a.find('A'))
 
-ultimo = a.rfind ('a')
-print('A posição do último A:', ultimo)
+ultimo = a.rfind ('A')
+print('A posição do último "A":', ultimo)
