4de50284e8b756caa4c87c11698e2ff27647c80d
Kellson <kelsonnb@gmail.com>
2026-01-09 16:22:57 -0300

    commit desafio027 edit1.2

--- desafio027.py
+++ desafio027.py
@@ -1,5 +1,5 @@
 nome=str(input('Digite seu nome completo:'))
 separo =  nome.split()
 
-print('Seu primeiro nome é:',separo[0])
+print('\nSeu primeiro nome é:',separo[0])
 print('Seu sobrenome é:',separo[-1])
