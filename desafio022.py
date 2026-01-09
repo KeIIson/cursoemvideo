6894190c2b1a98e08ea000c152858db433e73a51
Kellson <kelsonnb@gmail.com>
2026-01-09 16:48:18 -0300

    commit refinando desafio022 edit1.5

--- desafio022.py
+++ desafio022.py
@@ -4,8 +4,10 @@ print('Seu nome maiúsculas é: {}'.format(nome.upper()))
 print('Seu nome minúsculas é: {}'.format(nome.lower()))
 #print('Corta espaço:',nome.strip())
 #print('Separado:',nome.split())
-dividido=''.join(nome.split())#retirando os espaços no nome
+#dividido=''.join(nome.split())#retirando os espaços no nome
 #nome.split() #separa o nome em partes
 print('Seu nome ao todo tem {} letras:'.format(len(nome) - nome.count(' ')))
 print('Quantidade de Caracteres:',len(nome))
-print('Seu primeiro nome tem:{} letras'.format(nome.find(' ')))
+#print('Seu primeiro nome tem:{} letras'.format(nome.find(' ')))
+separa=nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras.'.format(separa[0],len(separa[0])))
