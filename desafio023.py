532de3ea219671bd48555ba06098f8bba46b321c
Kellson <kelsonnb@gmail.com>
2026-01-12 14:48:56 -0300

    commit refinando desafio023 edit1.7

--- desafio023.py
+++ desafio023.py
@@ -6,9 +6,20 @@ print('Dezena:',num[2])
 print('Unidade:',num[3])"""
 
 #Convertendo INT para STRING antes do resultado
-num=int(input('Digite um número entre 0 e 9999:'))
+"""num=int(input('Digite um número entre 0 e 9999:'))
 digitado=[int(n)for n in str(num)]
 print('Milhar:',digitado[0])
 print('Centena:',digitado[1])
 print('Dezena:',digitado[2])
-print('Unidade:',digitado[3])
+print('Unidade:',digitado[3])"""
+
+num  = int(input('Digite um número: '))
+u = num  // 1 % 10 # dessa forma os divisores apresentarão o valor por casa
+d = num // 10 % 10 #dezena
+c = num // 100 % 10 #centena
+m = num // 1000 % 10 #milhar
+print('Analisando o número {}'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
