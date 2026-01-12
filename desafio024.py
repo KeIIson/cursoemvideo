ace6fa9fbdfb845af89bb8eacace4e5260df4c2c
Kellson <kelsonnb@gmail.com>
2026-01-12 15:13:40 -0300

    commit refinando desafio024 edit1

--- desafio024.py
+++ desafio024.py
@@ -1,4 +1,3 @@
 print('Vou verificar se sua cidade começa com SANTO')
 santo=str(input('Digite o nome da sua cidade:')).strip()
-positivo = 'SANTO' in santo.upper()
-print('{}!'.format(positivo))
+print()
